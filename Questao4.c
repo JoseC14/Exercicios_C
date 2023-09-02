582d53ccd2bab5d28aeb41bc52377613bd6c31f5
JoseC14 <josec@DebianJC.DebianJC>
2023-09-02 19:37:05 -0300

    Adicionado questao 4 e 5

---
+++ Questao4.c
@@ -0,0 +1,36 @@
+//Faça um programa que leia um número inteiro positivo até que o usuário digite 0. Para cada número maior que zero, mostre o número e o fatorial do mesmo.
+#include <stdio.h>
+
+int main(){
+	
+    int num,res;
+	
+	
+	while(1){
+		res = 1;
+		printf("Digite o número para fazer o fatorial: ");
+		if(scanf("%i", &num) == 0){
+			printf("Entrada inválida\n");
+			getchar();
+			continue;
+		};
+		
+		if(num == 0){
+			break;
+		}else if(num < 0){
+			printf("Número negativo\n");
+			continue;
+		}else{
+			while(num !=0){
+				res *= num;
+				num--;
+			}
+			
+			printf("Resultado do Fatorial: %i \n", res);
+		}
+	
+	}
+	
+	printf("Fim do programa\n");
+	return 0;
+}
---
+++ Questao5.c
@@ -0,0 +1,54 @@
+
+
+/*
+Faça um programa que receba as idades, pesos e alturas de dez pessoas, calcule e mostre:
+
+1) a média das idades das dez pessoas;
+
+2) a quantidade de pessoas com peso superior a 80Kg e altura inferior a 1,50 metro;
+
+3) a porcentagem de pessoas com idade entre 10 e 30 anos entre as pessoas que medem mais de 1,90m.
+*/
+#include <stdio.h>
+
+int main(){
+	
+	int idade, quantPesoMetro, idades,altos,entre;
+	float peso, altura, porcetagem, idadeMedia;
+	quantPesoMetro = 0;
+	idades = 0;
+	for(int i = 0; i < 10 ; i++){
+		printf("Digite a idade da pessoa %i: ", i + 1);
+		scanf("%i",&idade);
+		
+		printf("Digite o peso: ");
+		scanf("%f",&peso);
+		
+		printf("Digite a altura: ");
+		scanf("%f", &altura);
+		
+		if(peso > 80 && altura < 1.50){
+			quantPesoMetro += 1;
+		}
+		
+		if(altura > 1.90){
+			altos += 1;	
+		}
+		
+		if((idade > 10 && idade < 30) && altura > 1.90){
+			 entre += 1;
+		}
+		
+		idades += idade;
+		printf("Soma das idades %i", idades);
+	}
+			
+	idadeMedia =  idades/10;
+	
+	porcetagem = (entre/altos) * 100;
+    printf("Média das idades %f \n", idadeMedia);
+	printf("%i pessoas com peso superior a 80Kg e altura inferior a 1,50 metro\n", quantPesoMetro);
+	printf("porcentagem de pessoas com idade entre 10 e 30 anos entre as pessoas que medem mais de 1,90m: %f\n", porcetagem);
+	
+	return 0;
+}
